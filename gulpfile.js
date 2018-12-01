8a9996896ab6e3d7f46f872e2d4e1eacfdb05c20
Fede <piruloequis@gmail.com>
2018-12-01 17:32:49 +0100

    added nested CSS and variables etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 //import gulp from 'gulp'; this is in ES6
-var gulp = require('gulp'), watch = require('gulp-watch');
+var gulp = require('gulp'),
+ watch = require('gulp-watch'),
+ postcss = require('gulp-postcss'),
+ autoprefixer = require('autoprefixer'),
+ cssvars = require('postcss-simple-vars'),
+ nested = require('postcss-nested');
 
 gulp.task('default', function() {
     console.log("You did it mate!");
@@ -10,7 +15,9 @@ gulp.task('html', function(){
 });
 
 gulp.task('styles', function(){
-    console.log("imagine Sass or PostCSS running here");
+   return gulp.src('./app/assets/styles/styles.css')
+   .pipe(postcss([cssvars, nested, autoprefixer({ browsers: ['>0%']})]))
+   .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
