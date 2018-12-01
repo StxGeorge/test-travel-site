a87e5e4ede10e7ab635ac0159359f273079a265b
Fede <piruloequis@gmail.com>
2018-12-01 23:54:01 +0100

    added css structure and automated imports

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
  postcss = require('gulp-postcss'),
  autoprefixer = require('autoprefixer'),
  cssvars = require('postcss-simple-vars'),
- nested = require('postcss-nested');
+ nested = require('postcss-nested'),
+ cssImport = require('postcss-import');
 
 gulp.task('default', function() {
     console.log("You did it mate!");
@@ -16,7 +17,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
    return gulp.src('./app/assets/styles/styles.css')
-   .pipe(postcss([cssvars, nested, autoprefixer({ browsers: ['>0%']})]))
+   .pipe(postcss([cssImport, cssvars, nested, autoprefixer({ browsers: ['>0%']})]))
    .pipe(gulp.dest('./app/temp/styles'));
 });
 
